3f02f6de87f9865dcfd3e2bc9b78e594389f253f
PriPh625 <philip99price@gmail.com>
2025-05-15 17:46:13 +0200

    Zeitpunkt für die Wettervorhersage anzeigen

--- main.js
+++ main.js
@@ -41,10 +41,12 @@ async function showForecast(latlng) {
 
    // Popup erzeugen
    let details = jsondata.properties.timeseries[0].data.instant.details;
+   let timestamp = new Date(jsondata.properties.meta.updated_at)
    let markup = `
+        <h3>Wettervorhersage für ${timestamp.toLocaleString()}</h3>
         <ul>
         <li>Luftdruck (hpa): ${details.air_pressure_at_sea_level}</li>
-        <li>Lufttemperatur (°C): ${details.air_temperatur}</li>
+        <li>Lufttemperatur (°C): ${details.air_temperature}</li>
         <li>Bewölkungsgrad (%): ${details.cloud_area_fraction}</li>
         <li>Luftfeuchtigkeit (%): ${details.relative_humidit}</li>
         <li>Windrichtung (°): ${details.wind_from_direction}</li>
